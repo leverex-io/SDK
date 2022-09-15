226119cab174476fbc46700689ec0c92b4bc04da
Ation <gation@gmail.com>
2022-09-15 10:26:16 +0300

    Allow all origins

--- bitfinex_dealer/bitfinex_dealer.py
+++ bitfinex_dealer/bitfinex_dealer.py
@@ -129,13 +129,12 @@ class HedgingDealer():
       self._app = FastAPI()
 
       origins = [
-          "http://localhost",
-          "http://localhost:8000"
+          '*'
       ]
 
       self._app.add_middleware(CORSMiddleware,
                                allow_origins=origins,
-                               allow_credentials=False,
+                               allow_credentials=True,
                                allow_methods=["*"],
                                allow_headers=["*"])
 
